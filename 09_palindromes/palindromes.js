35d46f60cefbdbca983fc8e1fb79f169af925010
Sarah Lopez <slopez323@gmail.com>
2022-02-24 14:37:01 -0500

    until 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let forwardStr = ""
+    let backwardStr = ""
+
+    for (let c of str) {
+        if (c.toLowerCase() === c.toUpperCase()) {
+            continue
+        } else {
+            forwardStr += c.toLowerCase()
+        }
+    }
+
+    backwardStr = [...forwardStr]
+    backwardStr = backwardStr.reverse()
+    backwardStr = backwardStr.join("")
+
+    if (forwardStr === backwardStr) {
+        return true
+    } else return false
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = +num
+    if (num === 1 || num === 2) {
+        return 1
+    } else if (num > 2) {
+        let oldNum = 1
+        let currNum = 1
+        for (let i = 3; i <= num; i++){
+            currNum += oldNum
+            oldNum = currNum - oldNum 
+        }
+        return currNum
+    } else return "OOPS"
+
 
 };
 
